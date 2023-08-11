1bdbb971c776b4598e3148c7dca732c763a3f042
Suresh-Gwala-Metacube <suresh.gwala@metacube.com>
2023-08-11 15:31:59 +0530

    Added latest code with new mysql file

--- web/sites/default/settings.php
+++ web/sites/default/settings.php
@@ -814,7 +814,7 @@ $settings['migrate_node_migrate_type_classic'] = FALSE;
 #   include $app_root . '/' . $site_path . '/settings.local.php';
 # }
 $databases['default']['default'] = array (
-  'database' => 'm150191_drupal',
+  'database' => 'wonka',
   'username' => 'root',
   'password' => 'passwd',
   'prefix' => '',
